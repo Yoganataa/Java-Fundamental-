43ea09f424f2d7340460c75d4e26df59103c0fbb
AVEY NATA <76590363+AVEYNT@users.noreply.github.com>
2022-12-05 09:44:18 +0700

    Learn Java 23%

---
+++ StrukturDasar/src/com/jota/javadundamental/basic/Main.java
@@ -0,0 +1,11 @@
+package com.jota.javadundamental.basic;
+
+import com.jota.javadundamental.basic.music.Guitar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+        Guitar.duar();
+    }
+
+}
---
+++ StrukturDasar/src/com/jota/javadundamental/basic/music/Guitar.java
@@ -0,0 +1,8 @@
+package com.jota.javadundamental.basic.music;
+
+public class Guitar {
+
+    public static void duar() {
+        System.out.println("Duarrrrrr");
+    }
+}
